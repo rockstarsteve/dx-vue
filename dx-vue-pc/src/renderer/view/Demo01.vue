cd38aad74ba6e04e1af63e7e44ee135f4824e15c
670139644@qq.com <670139644@qq.com>
2019-12-25 18:36:24 +0800

    添加Electron-Vue的demo，，测试桌面端应用开发

---
+++ dx-vue-pc/src/renderer/view/Demo01.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="content">
+        hello world
+    </div>
+</template>
+
+<script>
+    export default {
+      name: 'Demo01'
+    }
+</script>
+
+<style scoped>
+
+</style>
